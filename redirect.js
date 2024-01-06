17fb49bf83d14cf3a2e1116a88902c766a4352ed
Anh Hoang Nguyen <iam@hoanganh.dev>
2024-01-06 01:13:14 -0500

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -3,8 +3,10 @@ export default {
     const url = new URL(request.url);
     const keyPath = url.pathname.replace("/", "")
     let target = ""
+
     try {
       target = await env.LINKMAP.get(keyPath)
+      return Response.redirect(target);
     } catch (err) {
       return new Response(`
       <style>
@@ -16,10 +18,10 @@ export default {
       <div style="font-family: sans-serif; background: black; color: white; height: 100vh; display: flex; flex-direction: column; padding: 2rem;">
         <h1>Anh's URL Shortener</h1>
         <h2>Bad Request: Missing/Incorrect source pathname for redirection.</h2>
-        <h2>Error code: HTTP/400</h2>
+        <h2>Error code: HTTP/418</h2>
         <p>You have tried to request path: <code style="padding: 0.25rem 1rem; background: #242424;">${keyPath === "" ? "very null" : keyPath}</code>, which does not exist in my database :sadEmoji:</p>
-      </div>
-      `, { headers: { "Content-Type": "text/html" }, status: 400 });
+      </div>  
+      `, { headers: { "Content-Type": "text/html" }, status: 418 });
     }
 
     // if (!target) {
@@ -27,6 +29,5 @@ export default {
     // }
 
     // The Response class has static methods to create common Response objects as a convenience
-    return Response.redirect(target);
   },
 };
