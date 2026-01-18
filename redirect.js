d1a44bbdcb433fda91cdcf7a1e5e9db98be779e5
Anh Nguyen <anhnguyen@aaanh.com>
2026-01-17 19:31:17 -0500

    migrate over to github repo

--- redirect.js
+++ redirect.js
@@ -1,27 +1,47 @@
 export default {
   async fetch(request, env, ctx) {
     const url = new URL(request.url);
-    const keyPath = url.pathname.replace("/", "")
-    let target = ""
+    const keyPath = url.pathname.replace("/", "");
+    let target = "";
 
     try {
-      target = await env.LINKMAP.get(keyPath)
+      target = await env.LINKMAP.get(keyPath);
       return Response.redirect(target);
     } catch (err) {
-      return new Response(`
-      <style>
-        body {
-          margin: 0;
-          padding: 0;
-        }
-      </style>
-      <div style="font-family: sans-serif; background: black; color: white; height: 100vh; display: flex; flex-direction: column; padding: 2rem;">
-        <h1>Anh's URL Shortener</h1>
-        <h2>Bad Request: Missing/Incorrect source pathname for redirection.</h2>
-        <h2>Error code: HTTP/418</h2>
-        <p>You have tried to request path: <code style="padding: 0.25rem 1rem; background: #242424;">${keyPath === "" ? "very null" : keyPath}</code>, which does not exist in my database :sadEmoji:</p>
-      </div>  
-      `, { headers: { "Content-Type": "text/html" }, status: 418 });
+      return new Response(
+        `
+      <html>
+        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <script src="https://cdn.tailwindcss.com"></script>
+          <title>Anh's URL Shortener</title>
+          <meta name="description" content="A simple private URL shortener using Cloudflare Workers and KV."/>
+        </head>
+        <style>
+          body {
+            margin: 0;
+            padding: 0;
+          }
+        </style>
+        <div class="font-sans bg-black text-white h-screen flex flex-col p-8">
+          <h1 class="text-4xl">Anh's URL Shortener</h1>
+          <h2><b>Bad Request:</b> Missing/Incorrect source pathname for redirection.</h2>
+          <h2><b>Error code:</b> <code>HTTP/418</code></h2>
+          <p>You have tried to request path: <code style="padding: 0.25rem 1rem; background: #242424;">${
+            keyPath === "" ? "very null, like, nothing's there..." : keyPath
+          }</code>, which does not exist in my database :sadEmoji:</p>
+          <p>Try <a class="text-sky-500 hover:underline" href="https://anh.to/nohello">anh.to/nohello</a></p>
+          <br/>
+          <ul class="flex">
+            <li><a class="m-2 ml-0 p-2 bg-white/10 hover:bg-white/20 transition-all ease-in-out rounded-xl" href="https://aaanh.com">Homepage</a></li>
+            <li><a class="m-2 p-2 bg-white/10 hover:bg-white/20 transition-all ease-in-out rounded-xl" href="https://github.com/aaanh">Github</a></li>
+          </ul>
+        </div>
+      </html>  
+      `,
+        { headers: { "Content-Type": "text/html" }, status: 418 }
+      );
     }
 
     // if (!target) {
--- worker.js
+++ worker.js
@@ -8,7 +8,9 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
-import handleRedirect from './redirect.js';
+import handleProxy from "./proxy.js";
+import handleRedirect from "./redirect.js";
+import apiRouter from "./router.js";
 
 // Export a default object containing event handlers
 export default {
@@ -19,21 +21,32 @@ export default {
     const url = new URL(request.url);
 
     try {
-      return handleRedirect.fetch(request, env, ctx)
+      return handleRedirect.fetch(request, env, ctx);
     } catch (err) {
-      Response.redirect("https://aaanh.com")
+      Response.redirect("https://aaanh.com");
     }
 
-    
+    // // You can get pretty far with simple logic like if/switch-statements
+    // switch (url.pathname) {
+    //   case '/redirect':
+    //     return handleRedirect.fetch(request, env, ctx);
 
+    //   case '/proxy':
+    //     return handleProxy.fetch(request, env, ctx);
+    // }
 
-		// return new Response(
-		// 	`Try making requests to:
+    // if (url.pathname.startsWith('/api/')) {
+    //   // You can also use more robust routing
+    //   return apiRouter.handle(request);
+    // }
+
+    // return new Response(
+    // 	`Try making requests to:
     //   <ul>
     //   <li><code><a href="/redirect?redirectUrl=https://example.com/">/redirect?redirectUrl=https://example.com/</a></code>,</li>
     //   <li><code><a href="/proxy?modify&proxyUrl=https://example.com/">/proxy?modify&proxyUrl=https://example.com/</a></code>, or</li>
     //   <li><code><a href="/api/todos">/api/todos</a></code></li>`,
-		// 	{ headers: { "Content-Type": "text/html" } }
-		// );
+    // 	{ headers: { "Content-Type": "text/html" } }
+    // );
   },
 };
